0500c3a92ead141e19c331d47ba1510dfb8d89bb
Ruhaan Doshi <ruhaand@umich.edu>
2022-11-08 13:10:23 -0500

    add GET endpoints for retrieving existing files

--- src/api/audio.js
+++ src/api/audio.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path')
+const fs = require('fs')
 const router = express.Router();
 const multer = require('multer')
+const duration = require("wav-audio-length").default;
 
 const storage = multer.diskStorage({
   filename: function (req, file, cb) {
-    // console.log('filename')
+    // prevent duplicate names overwriting an existing file
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     cb(null, file.originalname)
   },
   destination: function (req, file, cb) {
@@ -16,6 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
+const directoryPath = path.join(__dirname, '../../uploads');
 
 router.get('/', (req, res) => {
   res.json(['😀', '😳', '🙄']);
@@ -23,7 +27,70 @@ router.get('/', (req, res) => {
 
 router.post('/upload', upload.single('file'), (req, res) => {
   console.log(req.file)
-  res.json("upload route hit");
+  if(req.file === undefined) {
+    res.status(500)
+    res.send("Nice try, you actually gotta upload a file.");
+  } else {
+    res.send("File successfully uploaded")
+  }
 });
 
-module.exports = router;
+router.get('/metadata', (req, res) => {
+  if(req.query.name) {
+    const name = req.query.name
+    const filePath = path.join(__dirname, `../../uploads/${name}`);
+    fs.readFile(filePath, 'binary', (err, content) => {
+      if (err) {
+        console.log(err)
+      }
+      let buffer = Buffer.from(content, 'binary');
+      let sec = duration(buffer);
+      res.send({buffer: buffer, length: sec})
+      console.log(sec);
+    });
+  }
+})
+
+router.get('/list', (req, res) => {
+  fs.readdir(directoryPath, (err, files) => {
+    //handling error
+    if (err) {
+      res.sendStatus(404);
+      res.send('Unable to scan directory: ' + err);
+    }
+    res.json({all_files: files})
+
+  });
+})
+
+router.get('/info', async (req, res, next) => {
+  // TODO(ruhaan): I'd like to implement a more robust error file system, where a malicious filename will not allow
+  //  unwanted access to files.
+
+  let options = {
+    root: path.join(__dirname, '../../uploads'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+
+  if(req.query.name) {
+    const fileName = req.query.name
+    res.sendFile(fileName, options, (err) => {
+      if(err) {
+        // res.sendStatus(404)
+        next(err)
+      } else {
+        console.log('Sent:', fileName)
+      }
+    })
+  } else {
+    // res.status(400);
+    res.send("No filename provided!");
+  }
+
+})
+
+module.exports = router
